646db7767a477bc718e836a2961b649571f9aa84
ErnestoDof23 <20233tn194@utez.edu.mx>
2025-09-01 23:12:14 -0600

    Añadi otra linea

--- SayHello.java
+++ SayHello.java
@@ -2,6 +2,8 @@ package mx.edu.utez;
 
 public class SayHello {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("Neaaaar");
     }
 }
